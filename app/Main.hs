1b9ac24a642f3bad8156b764f8a060ed2f2c3107
ishiy <y.ishihara.1993@gmail.com>
2017-02-28 13:37:09 +0900

    Add trans

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
 import Lib
+import Syntax
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+    let exp1' = trans exp1
+        exp2' = trans exp2
+    putStr "exp1  = "
+    putStrLn $ pshow exp1
+    putStr "      = "
+    print $ eval exp1
+    putStr "exp1' = "
+    putStrLn $ pshow exp1'
+    putStr "      = "
+    print $ eval exp1'
+    putStr "exp2  = "
+    putStrLn $ pshow exp2
+    putStr "      = "
+    print $ eval exp2
+    putStr "exp2' = "
+    putStrLn $ pshow exp2'
+    putStr "      = "
+    print $ eval exp2'
--- src/Syntax.hs
+++ src/Syntax.hs
@@ -30,6 +30,9 @@ mul x y = inn $ Mul x y
 exp1 :: Exp
 exp1 = add (mul (val 2) (val 3)) (add (val 2) (val 2))
 
+exp2 :: Exp
+exp2 = mul (val 3) (add (val 2) (val 4))
+
 class Eval fs where
     evalAlg :: Matches fs Int Int
 
@@ -84,3 +87,92 @@ showAdd (Add n1 n2) = "(" ++ n1 ++ " + " ++ n2 ++ ")"
 showMul :: Mul String -> String
 showMul (Mul n1 n2) = "(" ++ n1 ++ " * " ++ n2 ++ ")"
 
+trans :: Exp -> Exp
+trans = fold tr
+
+tr :: Algebras '[Val, Add, Mul] Exp
+tr = trVal ::: trAdd ::: trMul ::: Void
+
+trVal :: Val Exp -> Exp
+trVal (N n) = val n
+
+trAdd :: Add Exp -> Exp
+trAdd (Add e1 e2) = add (trans e1) (trans e2)
+
+trMul :: Mul Exp -> Exp
+trMul (Mul e1 e2) 
+    | isVal e1 && isVal e2 = if isZero e2
+                                then val 0
+                                else add e1 (trans $ mul e1 (sub1 e2))
+    | isVal e1 && isAdd e2 = let Just (Add e21 e22) = prj e2
+                             in  add (trans $ mul e1 e21)
+                                     (trans $ mul e1 e22)
+    | isAdd e1 && isVal e2 = let Just (Add e11 e12) = prj e1
+                             in  add (trans $ mul e11 e2)
+                                     (trans $ mul e12 e2)
+    | isAdd e1 && isAdd e2 = let Just (Add e11 e12) = prj e1
+                                 Just (Add e21 e22) = prj e2
+                             in  add (add (trans $ mul e11 e21)
+                                          (trans $ mul e11 e22))
+                                     (add (trans $ mul e12 e21)
+                                          (trans $ mul e12 e22))
+    | otherwise = trans $ mul (trans e1) (trans e2)
+
+sub1 :: Exp -> Exp
+sub1 e = let Just (N n) = prj e
+         in if n > 0 then val (n-1) else val (n+1)
+
+isZero :: Exp -> Bool
+isZero e = let Just (N n) = prj e
+           in n == 0
+
+
+isVal :: Exp -> Bool
+isVal = match $ (\(N _) -> True)
+            ::: (\(Add _ _) -> False)
+            ::: (\(Mul _ _) -> False)
+            ::: Void
+
+isAdd :: Exp -> Bool
+isAdd = match $ (\(N _) -> False)
+            ::: (\(Add _ _) -> True)
+            ::: (\(Mul _ _) ->  False)
+            ::: Void
+
+-- 必要なもの: 関数
+-- f :: Mul (Fix '[Val, Add]) -> Fix '[Val, Add]
+-- これを実装するにはかけ算の非再帰的定義が必要
+--
+-- これがあれば、
+-- desugarAlg = f ::: transAlg
+
+-- desugarAlg :: (gs <: gs, Mem Add gs, Mem Val gs)
+--            => Algebras (Mul ': gs) (Fix gs)
+-- desugarAlg = (\(Mul e1 e2) -> fold (reduceAlg e1) e2) ::: transAlg
+
+-- desugar :: (fs <: fs, Mem Add fs, Mem Val fs)
+--         => Fix (Mul ': fs) -> Fix fs
+-- desugar = fold desugarAlg
+
+
+-- class Reduce gs where
+--     reduceAlg :: Fix gs -> Algebras gs (Fix gs)
+    -- reduceAlg :: (gs <: gs, Mem Add gs, Mem Val gs)
+    --           => Fix gs -> Algebras gs (Fix gs)
+
+-- instance Reduce ('[]) where
+--     reduceAlg _ = Void
+
+-- instance Reduce fs => Reduce (Val ': fs) where
+--     reduceAlg m = reduceVal m ::: reduceAlg m
+
+-- instance Reduce fs => Reduce (Add ': fs) where
+--     reduceAlg m = reduceAdd m ::: reduceAlg m
+
+-- reduceVal :: (fs <: fs, Mem Add fs, Mem Val fs)
+--           => Fix fs -> Val (Fix fs) -> Fix fs
+-- reduceVal m (N n) = add m (val n)
+
+-- reduceAdd :: (fs <: fs, Mem Add fs, Mem Val fs)
+--           => Fix fs -> Add (Fix fs) -> Fix fs
+-- reduceAdd m (Add n1 n2) = add m (add n1 n2)
